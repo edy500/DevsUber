e624725acdd4b4ae90f0e5a7e339dd94e48ff4fb
edy500 <36477658+edy500@users.noreply.github.com>
2019-12-22 21:37:41 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,115 +1,12 @@
-import React, { useState } from 'react'
-import { StatusBar, Platform, Text } from 'react-native'
-import { StackActions, NavigationActions } from 'react-navigation'
-import { connect } from 'react-redux'
-import useDevsUberApi from '../../useDevsUberApi'
+import React from 'react'
+import { StatusBar, SafeAreaView, Text } from 'react-native'
 
-import { Container, Header, HeaderTitle,
-         Menu, MenuItem, MenuItemText, Input, 
-         ActionButton, ActionButtonText
-         } from './styled'
-
-const Page = (props) => {
-  const api = useDevsUberApi()
-
-
-  const [activeMenu, setActiveMenu] = useState('signin')
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [error, setError] = useState('')
-
-  //const res = await api.signin(email, password)
-
-  const handleSignIn = async () => {
-    if(email && password) {
-      const res = await api.signin(email, password)
-
-      if(res.error) {
-        alert(res.error)
-
-      } else {
-        props.setToken(res.token)
-        // 2. redirecionar para o Home
-
-      }
-
-    }
-
-  }
-
-  const handleSignUp = async () => {
-    if(name && email && password)  {
-      const res = await api.signup(name, email, password)
-
-      if(res.error) {
-        alert(res.error)
-
-      } else {
-        props.setToken(res.token)
-        // 2. redirecionar para o Home
-
-      }
-    }
-
-
-    }
-
-
-
-
+const Page = () => {
     return (
-      <Container behavior={Platform.OS === 'ios'?'padding':null}>
-        <StatusBar barStyle="light-content"/>
-        <Header>
-<HeaderTitle>DevsUber</HeaderTitle>
-        </Header>
-        <Menu>
-          <MenuItem active={activeMenu=='signin'} onPress={()=>setActiveMenu('signin')} underlayColor="transparent">
-            <MenuItemText>Login</MenuItemText>
-          </MenuItem>
-          <MenuItem active={activeMenu=='signup'} onPress={()=>setActiveMenu('signup')} underlayColor="transparent">
-            <MenuItemText>Cadastrar</MenuItemText>
-          </MenuItem>
-        </Menu>
-
-        {activeMenu == 'signup' &&
-        <Input value={name} onChangeText={t=>setName(t)} placeholder="Nome" placeholderTextColor="#999"/>
-        }
-
-        <Input value={email} onChangeText={t=>setEmail(t)} keyboardType="email-address" autoCapitalize="none" placeholder="E-mail" placeholderTextColor="#999"/>
-        
-        <Input value={password} onChangeText={t=>setPassword(t)}  placeholder="Senha" placeholderTextColor="#999" secureTextEntry={true}/>
-
-        {activeMenu == 'signin' &&
-        <ActionButton onPress={handleSignIn}>
-          <ActionButtonText>Login</ActionButtonText>
-        </ActionButton> 
-        }
-
-        {activeMenu == 'signup' &&
-        <ActionButton onPress={handleSignUp}>
-          <ActionButtonText>Cadastrar</ActionButtonText>
-        </ActionButton> 
-        }
-
-        <Text>TOKEN: {props.token} </Text>
-      
-      </Container>
+        <SafeAreaView>
+            <StatusBar barStyle="dark-content" />
+            <Text>...</Text>
+        </SafeAreaView>
     )
-  }
-
-
-  const mapStateToProps = (state) => {
-    return {
-        token:state.userReducer.token
-    }
 }
-const mapDispatchToProps = (dispatch) => {
-  return {
-    setToken:(token)=>dispatch({type:'SET_TOKEN', payload:{token}})
-  }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(Page)
-
-  //export default Page
+export default Page
